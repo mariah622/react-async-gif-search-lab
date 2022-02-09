45f773745c8086728f794a6fe23072ac983ae1b1
mariah622 <mjustine622@gmail.com>
2022-02-09 15:40:04 -0500

    Done.

--- src/components/GifList.js
+++ src/components/GifList.js
@@ -1,15 +1,18 @@
 import React from 'react'
 
 
-const GifList = (props) => {
-    return (
-        <div>
-            {props.gifs.map(gif => <img key={gif.url} src={gif.url} alt='gif'/>)}
+class GifList extends React.Component {
+    render(){
+        return (
+            <div>
+            {this.props.gifs.map(gif => <img key={gif.url} src={gif.url} alt='gif'/>)}
         </div>
-    )
-    // this returns our gifs to the container after search is inputted
-    
+        )
+
+    }
+        
 
 }
+
 
 export default GifList
